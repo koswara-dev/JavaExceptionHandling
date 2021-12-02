993a0f14d853de74a14656f7a41ab8db35d032fa
Koswara <koswara.c79@gmail.com>
2021-12-02 15:31:28 +0700

    coba ubah

--- src/ImplBebasException.java
+++ src/ImplBebasException.java
@@ -2,7 +2,20 @@
 public class ImplBebasException {
 
 	public static void main(String[] args) {
-		
+		String jk="wanita", status="single";
+		if(jk=="pria") {
+			if(status=="single") {
+				System.out.println("pajak 10%");
+			} else {
+				System.out.println("pajak 5%");
+			}
+		} if (jk=="wanita") {
+			if (status=="single") {
+				System.out.println("pajak 7%");
+			} else {
+				System.out.println("pajak 3%");
+			}
+		}
 	}
 	
 }
